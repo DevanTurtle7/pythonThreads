aecd82b704616b4aad434a7982455afd21759789
Devan Kavalchek <devankav7@gmail.com>
2021-08-31 16:15:44 -0400

    added comments

--- pyThreads.py
+++ pyThreads.py
@@ -24,12 +24,12 @@ def benchmark(thread, num_threads):
         thread: The thread that is preforming this chunk of the work
         num_threads: The number of threads being used for this benchmark
     """
-    avg = math.floor(NUM_ITERATIONS / num_threads)
-    start = avg * thread
-    end = start + avg if thread + 1 != num_threads else NUM_ITERATIONS
+    avg = math.floor(NUM_ITERATIONS / num_threads) # Calculate the average work that should be done by 1 thread
+    start = avg * thread # Get the starting index for the loop
+    end = start + avg if thread + 1 != num_threads else NUM_ITERATIONS # Calculate the ending index for the loop unless it is the last thread. If it is the last thread, assign the end to the end of the operations. This is because the end index calculation looses some numbers to rounding.
     
-    for i in range(start, end):
-        math.exp(random.random())
+    for i in range(start, end): # Iterate the given amount of times
+        math.exp(random.random()) # Preform an arbitrary operation
 
 def time_trial(n):
     start_time = time.time()
