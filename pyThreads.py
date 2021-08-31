6ddaa7479633fd01d3e4f6b1e2a08dd6b96e6b2e
Devan Kavalchek <devankav7@gmail.com>
2021-08-31 16:40:39 -0400

    added cli

--- pyThreads.py
+++ pyThreads.py
@@ -10,9 +10,7 @@ import math
 import random
 import time
 
-NUM_ITERATIONS = 10000000 # The number of iterations/operations in a benchmark
-
-def benchmark(thread, num_threads):
+def benchmark(thread, num_threads, num_operations):
     """
     A function that preforms many arbitrary operations so that are intensive
     on the system. The operations are split into chunks depending on how many
@@ -23,15 +21,16 @@ def benchmark(thread, num_threads):
     Parameters:
         thread: The thread that is preforming this chunk of the work
         num_threads: The number of threads being used for this benchmark
+        num_operations: The number of operations to be preformed
     """
-    avg = math.floor(NUM_ITERATIONS / num_threads) # Calculate the average work that should be done by 1 thread
+    avg = math.floor(num_operations / num_threads) # Calculate the average work that should be done by 1 thread
     start = avg * thread # Get the starting index for the loop
-    end = start + avg if thread + 1 != num_threads else NUM_ITERATIONS # Calculate the ending index for the loop unless it is the last thread. If it is the last thread, assign the end to the end of the operations. This is because the end index calculation looses some numbers to rounding.
+    end = start + avg if thread + 1 != num_threads else num_operations # Calculate the ending index for the loop unless it is the last thread. If it is the last thread, assign the end to the end of the operations. This is because the end index calculation looses some numbers to rounding.
     
     for i in range(start, end): # Iterate the given amount of times
         math.exp(random.random()) # Preform an arbitrary operation
 
-def time_trial(num_threads):
+def time_trial(num_threads, num_operations):
     """
     Preforms a timed trial of the benchmark function using a given number of
     threads.
@@ -44,7 +43,7 @@ def time_trial(num_threads):
 
     # Create and start the given number of threads
     for thread in range(0, num_threads): # Iterate num_threads times
-        current = mp.Process(target=benchmark, args=(i, num_threads)) # Create a new thread
+        current = mp.Process(target=benchmark, args=(thread, num_threads, num_operations)) # Create a new thread
         current.start() # Start the thread
         threads.append(current) # Add the threads to the threads array
 
@@ -55,8 +54,38 @@ def time_trial(num_threads):
     end_time = time.time() # Get the ending time
     time_taken = end_time - start_time # Get the time taken
 
-    print("Time taken with", num_threads, "threads:", time_taken) # Print a message
+    print("Time taken with", num_threads, "threads:", time_taken, "\n") # Print a message
+
+def prompt_user(query):
+    response = input(query)
+
+    try:
+        num = int(response)
+
+        if num > 0:
+            return num
+        else:
+            print("Invalid response. The number must be greater than 0.\n")
+            return prompt_user(query)
+    except:
+        if response == "exit":
+            return None
+        else:
+            print("Invalid response. Enter a number or type 'exit' to exit the program.\n")
+            return prompt_user(query)
 
 if __name__ == "__main__":
-    for i in range(1, 7): # Create 6 threads
-        time_trial(i) # Run a time trial with i threads
+    exit = False
+    
+    num_operations = prompt_user("Enter a number of operations (suggested: 10000000): ")
+
+    if (num_operations is None):
+        exit = True
+
+    while (not exit):
+        num_threads = prompt_user("Enter the number of threads to test: ")
+
+        if num_threads is not None:
+            time_trial(num_threads, num_operations)
+        else:
+            exit = True
