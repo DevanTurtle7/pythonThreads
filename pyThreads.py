de2176693c265b59f99a3f99c2d1f68e2dd96a9a
Devan Kavalchek <devankav7@gmail.com>
2021-08-31 16:13:12 -0400

    added comments and refactored

--- pyThreads.py
+++ pyThreads.py
@@ -1,14 +1,32 @@
+"""
+A random playground to mess around with threads and learn how they work in
+python.
+
+author: Devan Kavalchek
+"""
+
 import multiprocessing as mp
 import math
 import random
 import time
 
-NUM_ITERATIONS = 10000000
+NUM_ITERATIONS = 10000000 # The number of iterations/operations in a benchmark
 
-def benchmark(n, num_threads):
+def benchmark(thread, num_threads):
+    """
+    A function that preforms many arbitrary operations so that are intensive
+    on the system. The operations are split into chunks depending on how many
+    threads are being used. These operations take a long time to complete
+    (because there are so many), so that time differences are noticed between 
+    different number of threads.
+
+    Parameters:
+        thread: The thread that is preforming this chunk of the work
+        num_threads: The number of threads being used for this benchmark
+    """
     avg = math.floor(NUM_ITERATIONS / num_threads)
-    start = avg * n
-    end = start + avg if n + 1 != num_threads else NUM_ITERATIONS
+    start = avg * thread
+    end = start + avg if thread + 1 != num_threads else NUM_ITERATIONS
     
     for i in range(start, end):
         math.exp(random.random())
